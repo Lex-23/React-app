26d84964d3cca67657e4efda2742d97be3dffb28
Lex-23 <Liverpoolsmile23@gmail.com>
2021-03-24 00:31:55 +0300

    worked app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react';
 import TodoList from './Todo/TodoList';
 import Context from './context'
-import AddTodo from './Todo/AddTodo';
+import Loader from './loader'
+import loader from './loader'
+
+const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
+
 
 function App() {
   const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
 
   useEffect(() => {
     fetch ('https://jsonplaceholder.typicode.com/todos?_limit=5')
@@ -12,6 +17,7 @@ function App() {
     .then(todos => {
       setTimeout(() => {
         setTodos(todos)
+        setLoading(false)
       }, 2000)
       
     })
@@ -45,12 +51,16 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className='wrapper'>
         <h1>Total buy list</h1>
-        <AddTodo onCreate={addTodo} />
-
+        
+        <React.Suspense fallback={<p>Loading....</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+                
+        {loading && <Loader />}
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo}/>
           ) : (
-          <p>You haven't todos</p>
+          loading ? null : <p>You haven't todos!</p>
           )}
         
       </div>
